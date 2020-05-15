b57f1abcda4fc512719157ad15d625f65b179d3d
kawamataryou <ba068082@gmail.com>
2020-05-15 16:30:06 +0900

    init project

---
+++ main.ts
@@ -0,0 +1,23 @@
+import { createApp } from "https://servestjs.org/@v1.0.0/mod.ts";
+import * as flags from "https://deno.land/std/flags/mod.ts";
+
+const DEFAULT_PORT = 8080;
+const argPort = flags.parse(Deno.args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
+
+if (isNaN(port)) {
+  console.error('Port is not a number.');
+  Deno.exit(1);
+}
+
+const app = createApp();
+app.handle("/", async (req) => {
+  await req.respond({
+    status: 200,
+    headers: new Headers({
+      "content-type": "text/plain",
+    }),
+    body: "Hello, Servest!",
+  });
+});
+app.listen({ port });
